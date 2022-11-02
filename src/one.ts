6ad0c3b1d33a051c52dc8fbb01106e3e2f767e77
Earl Grey <orniton@gmail.com>
2022-11-02 21:29:04 +0100

    Initial commit

---
+++ src/one.ts
@@ -0,0 +1,9 @@
+console.log("ie ipsum reversus ab viral inferno, nam rick grimes malum cerebro.
+ De carne lumbering animata corpora quaeritis. Summus brains sit morbo vel maleficia.
+  De apocalzpsi gorger omero undead survivor dictum mauris.
+   Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.
+    Qui animated corpse, cricket bat max brucks terribilem incessu yombz.
+     The voodoo sacerdos flesh eater,exe fiat Jeff becum suscitat mortuos comedere carnem virus.
+      Yonbi tattered for solum oculi eorum defunctis go lum cerebro.
+       Nescio brains an Undead yombies.Sicut malus putrid voodoo horror.
+        Nigh tofth eliv ingdead.")
